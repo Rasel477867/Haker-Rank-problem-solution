863b1718e815519c328b08355bd7bc56d9ca0209
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-03-01 04:39:09 +0000

    Maximum Element-solution
    https://www.hackerrank.com/challenges/maximum-element/problem?isFullScreen=true

--- code.cpp
+++ code.cpp
@@ -4,65 +4,106 @@
 #include <iostream>
 #include<bits/stdc++.h>
 #define endl '\n'
-#define ll  unsigned long long int
+#define ll long long int
 #define pb push_back
+
+
 using namespace __gnu_pbds;
 using namespace std;
 
 typedef tree<long long int, null_type, less_equal<long long int>, rb_tree_tag,
         tree_order_statistics_node_update>
         ordered_multiset;
-ordered_multiset s;
+//ordered_multiset s3;
 //ordered_multiset :: iterator it;
+const ll mod=1e9+7;
+
+const ll N=1e1;
+bool sive[N];
+vector<ll>prime;
 vector<ll>v;
-map<char,ll>m;
-int ar[32];
-ll fun(ll num)
+stack<ll>s;
+map<ll,ll >m;
+map<ll,ll >:: iterator it;
+
+long long int gcd(long long int a,long long int b)
 {
-    ll i=0;
-    while(num!=1)
+    long long int c;
+    while(a%b!=0)
     {
-        num/=2;
-        i++;
+        c=b;
+        b=a%b;
+        a=c;
     }
-   return i;
+    return b;
 }
+double cal(double x)
+{
+    double y=sqrt(x);
+    return(x*x+2*y);
+}
+
+
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll n,a,c,i,t;
-    cin>>t;
-    while(t--)
+    ll i,j;
+    for(i=0; i<N; i++)
     {
-        cin>>n;
-        c=0;
-        while(n!=1)
+        sive[i]=true;
+    }
+    sive[0]=false;
+    sive[1]=false;
+    for(i=2; i*i<=N; i++)
+    {
+        if(sive[i])
         {
-            if(n&(n-1))
+            for(j=i*i; j<N; j+=i)
             {
-            a=fun(n);
-            ll b=1<<a;
-            n=n-b;
+                sive[j]=false;
             }
-            else{
-
-             n=n>>1;
-            }
-            c++;
-
-        }
-        if(c%2==1)
-        {
-            cout<<"Louise"<<endl;
-        }
-        else{
-            cout<<"Richard"<<endl;
         }
     }
+    for(i=0; i<N; i++)
+    {
+        if(sive[i]==true)
+            prime.pb(i);
+    }
+
+    /*start main funciton*/
+   ll t,n,a,b,c;
+   cin>>t;
+   while(t--)
+   {
+       cin>>a;
+       if(a==1)
+       {
+           cin>>b;
+           m[b]++;
+           s.push(b);
+
+       }
+       else if(a==2)
+       {
+           c=s.top();
+           if(m[c]==1)
+            m.erase(c);
+           else
+            m[c]--;
+           s.pop();
+       }
+       else
+       {
+           it=m.end();
+           it--;
+           cout<<it->first<<endl;
+       }
+   }
 
 
-
-  return 0;
+    return 0;
 }
+
